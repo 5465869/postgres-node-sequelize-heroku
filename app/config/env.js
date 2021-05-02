01e890ea9969ed43e5cc14dd521b1e63988bb5a7
colespencer1453 <css.csce1453@gmail.com>
2021-05-02 18:02:17 -0500

    hello

--- app/config/env.js
+++ app/config/env.js
@@ -1,8 +1,8 @@
 const env = {
-  database: 'd19it75c9j04j2',
-  username: 'lxknzbbcpmmdyl',
-  password: '3f1a281360fc9c4a5c42f35efeb94643064caa4293c74b136e885cc7c3d57beb',
-  host: 'ec2-54-163-245-64.compute-1.amazonaws.com',
+  database: 'd2p01d5em5jcbj',
+  username: 'ektwseouqsljwm',
+  password: '4314e96456da716a703491362d2c8eb192c99e7ce87c3ca9ba1d1a9b89b8f73a',
+  host: 'ec2-54-164-238-108.compute-1.amazonaws.com',
   port: 5432,
   dialect: 'postgres',
   pool: {
